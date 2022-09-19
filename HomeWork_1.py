ffec8b2324b27af2590db617901aa4e7346e942b
ItsFeci <113523642+ItsFeci@users.noreply.github.com>
2022-09-19 21:12:02 +0200

    Update HomeWork_1.py

--- HomeWork_1.py
+++ HomeWork_1.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python
 # coding: utf8
-from colorama import Fore
-from colorama import Style
-
-
-def main():
-#1.feladat
-    ## Szoveg bekerese majd betuk megszamolasa
+def string_modositas(input_text):
     letters = {}
-    input_text = input(f"1.(feladat)\nAdjon meg egy mondatot:\n{Fore.GREEN}")
-
     for i in input_text:
         if i in letters: 
             letters[i] = letters[i] + 1
         else:
             letters[i] = 1
     
-    print(f"{Fore.RESET}Betuk gyakorisaga:  {str(letters)}")
+    print(f"Betuk gyakorisaga:  {str(letters)}")
     ## Betuk forditva kiirasa:
     reverse_text = input_text[::-1]
     print(f"Forditva:  {reverse_text}")
@@ -24,22 +16,22 @@ def main():
     split_words = input_text.split(' ')
     print(f"Listaba rendeyve szavankent:  {split_words}\n")
 
-######################################################################################
-#2.feladat
-    number = int(input(f"(2.feladat)\nAdjon meg egy szamot es egy mertekegyseget (cm/inch):\nSzam:{Fore.GREEN} "))
-    unit = input(f"{Fore.RESET}mertekegyseg:{Fore.GREEN} ")
-    
-
+def cm_to_inch(number, unit):
     if unit == "cm":
-            print(f"{Fore.RESET}{number}cm = {number/2.54}inch")
+            print(f"{number}cm = {number/2.54}inch")
     elif unit == "inch":
-        print(f"{Fore.RESET}{number}inch = {number*2.54}cm{Fore.RESET}")
+        print(f"{number}inch = {number*2.54}cm")
     else:
-        print(f"{Fore.RESET}Not Correct Unit!")
+        print("Not Correct Unit!")
 
-
-
-   
 
 if __name__ == "__main__":
-	main()
+
+    user_text = input("Adjon meg egy mondatot:\n")
+    string_modositas(user_text)
+    
+
+    bekert_szam = int(input("Adjon meg egy szamot es egy mertekegyseget (cm/inch):\nSzam: "))
+    bekert_mertekegyseg = input("mertekegyseg: ")
+    cm_to_inch(bekert_szam,bekert_mertekegyseg)
+    
